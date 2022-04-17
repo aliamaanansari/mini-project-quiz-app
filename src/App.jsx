5589f0b9cb2cc26b986770330a165b5185966d11
Ali aman Ansari <aliamaan071@gmail.com>
2022-04-18 04:13:21 +0530

    import error

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Questions from "./pages/Questions";
+import Rules from "./pages/RulesPage";
 import Quizzes from "./pages/Quizzes";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
@@ -19,7 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/quizzes" element={<Quizzes />} />
           <Route path="/quiz/:quizId" element={<Questions />} />
-          <Route path="/rules" element={<RulesPage />} />
+          <Route path="/rules" element={<Rules />} />
           <Route path="/quiz/:quizId/result/" element={<Result />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<Signup />} />
